c14f2b85e34bc9019f7b753b6622f81e0f31fc3a
hasmuddin <hasmuddin@knockri.com>
2021-07-08 10:54:32 +0530

    js variables learned

---
+++ variable.js
@@ -0,0 +1,22 @@
+/* 1. printing a value */
+
+// console.log("hello i am from js");
+
+var digit = 5;
+var name = "Enamul Haque";
+var floatNumber = 2.3;
+
+/*2 . naming convention in variables   */
+
+// var digitHello = "hehehh"
+
+// function name => camelCase
+
+var number1 = 10;
+var number2 = 10;
+
+var result = number2 % number1;
+
+var isEqual = number1 >= number2;
+
+console.log(isEqual);
